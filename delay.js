220ffb06b06a2019aa7f1e6aa1a14578c6081107
Raj Chandra <raj.chandra2016@vitstudent.ac.in>
2017-02-25 09:40:33 +0530

    new updates to the UI section

---
+++ delay.js
@@ -0,0 +1,8 @@
+function delay(){
+	document.getElementById("throw_output").className='main bounceInRight animated';
+	document.getElementById("body").style.background='white';
+	document.getElementById("header").setAttribute("style","margin-top:20% ;color: #104E8B;");
+	document.getElementById("line").setAttribute("style","color:#104E8B; margin-left:40%;");
+	document.getElementById("throw_output").style.color="#104E8B";
+	document.getElementById("throw_output").innerHTML='LINgo';
+}
