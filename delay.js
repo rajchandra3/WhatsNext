ed008d1cdea07035e35868e8bf45881a134aab33
Raj Chandra <raj.chandra2016@vitstudent.ac.in>
2017-02-25 11:05:15 +0530

    UI more beautiful

--- delay.js
+++ delay.js
@@ -1,8 +1,8 @@
 function delay(){
 	document.getElementById("throw_output").className='main bounceInRight animated';
 	document.getElementById("body").style.background='white';
-	document.getElementById("header").setAttribute("style","margin-top:20% ;color: #104E8B;");
-	document.getElementById("line").setAttribute("style","color:#104E8B; margin-left:40%;");
+	document.getElementById("header").setAttribute("style","color: #104E8B;");
+	document.getElementById("line").setAttribute("style","color:#104E8B;");
 	document.getElementById("throw_output").style.color="#104E8B";
-	document.getElementById("throw_output").innerHTML='LINgo';
+	document.getElementById("throw_output").innerHTML='LinGo';
 }
