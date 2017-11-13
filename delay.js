506f06b9cd2c6073a830b916ac41fb0108e7fa90
Raj Chandra <raj.chandra2016@vitstudent.ac.in>
2017-11-14 00:35:19 +0530

    added new task

--- delay.js
+++ delay.js
@@ -4,5 +4,5 @@ function delay(){
 	document.getElementById("header").setAttribute("style","color: #104E8B;");
 	document.getElementById("line").setAttribute("style","color:#104E8B;");
 	document.getElementById("throw_output").style.color="#104E8B";
-	document.getElementById("throw_output").innerHTML='LinGo';
+	document.getElementById("throw_output").innerHTML='Online Book Exchange Platform(OBEP) for VIT';
 }
